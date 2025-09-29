1b007be4cae25f9a11b7a601f46953937931da61
Muazu Joseph <josephmuazu5@gmail.com>
2025-09-29 13:32:00 +0100

    initial commit - Task Management API with AUTH

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from users.views import RegisterView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/register/', RegisterView.as_view(), name='register'),
 ]
